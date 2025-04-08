8315d9bbb903c0477b46da9f3690d9bbf6ac36d9
arnV17 <arnavog@gmail.com>
2025-04-08 17:36:15 +0530

    new

--- Pokedex/pokedex-app/src/components/SideNav.jsx
+++ Pokedex/pokedex-app/src/components/SideNav.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
 
 import { first151Pokemon, getPokedexNumber } from '../index.js';
 
 export function SideNav(props) {
     const {pokenum , setPokenum} =props
+    const [pokemonList, setPokemonList] =  useState(first151Pokemon);
+    const [searchTerm, setSearchTerm] = useState('');
     
     console.log('Header component rendered');
     return (
         <div className='side-nav'>
             <h1 style={{ textAlign: "center", color: "#000000", fontSize: "2.5em", margin: "20px 0" }}>Pokédex</h1>
-            <input type="text" placeholder="Search Pokémon" />
-            {first151Pokemon.map((poki, index) => (
+            <input type="text" value={searchTerm} onChange={(e)=> setSearchTerm(e.target.value)  }placeholder="Search Pokémon" />
+            {pokemonList.map((poki, index) => (
                 <button className="nav-button" key={index} onClick={()=>{
                     
                     console.log("ONLICKKK");
